80ef60c76afec3e8164ad6b2092806f2470a7d71
Whitenessly <whitenessly@gmail.com>
2026-05-31 20:31:42 +0700

    .

---
+++ fe/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center text-3xl font-bold'> 
+        Welcome to Melodies Web System
+    </div>
+    )
+}
+
+export default Home
